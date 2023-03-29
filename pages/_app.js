e96c8f3b0280fd1915b7322486862c433c353045
amarmanhala <amar.manhala@gmail.com>
2023-03-29 13:32:29 -0400

    Add google fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Kaisei_Decol, Inter } from "next/font/google";
+
+const kaisei = Kaisei_Decol({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={kaisei.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
