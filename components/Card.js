5b0556cf6dc902391f522156772c484ce74ee299
amarmanhala <amar.manhala@gmail.com>
2023-04-07 18:09:19 -0400

    Designing...

--- components/Card.js
+++ components/Card.js
@@ -4,12 +4,12 @@ const Card = (props) => {
   console.log(props);
   const { name, description, tags } = props.projectData;
   return (
-    <div>
-      <h4>{name}</h4>
-      <p>{description}</p>
+    <div className="rounded-md mt-6 p-8 bg-[#F6F5F4]">
+      <h4 className="font-bold text-xl mb-2">{name}</h4>
+      <p className="paragraph mb-6">{description}</p>
       <div>
         { tags.map((eachTags, index) => {
-          return (<span key={index}>{eachTags}</span>)
+          return (<span key={index} className="px-1 py-0.5 border border-black rounded-md text-sm mr-2">{eachTags}</span>)
         }) }
       </div>
     </div>
--- components/Projects.js
+++ components/Projects.js
@@ -6,17 +6,20 @@ import Card from "./Card";
 const Projects = () => {
   return (
     <section className="mt-12">
+      <header className="mb-8">
       <h1 className={`text-4xl font-bold leading-relaxed ${kaisei.className}`}>
         Projects
       </h1>
       <p className={`paragraph ${ kaisei.className }`}>Below you’ll find a selection of my favourite projects and experiences.</p>
+      </header>
 
       <div>
+        
         {PROJECTS.map(eachProject => {
           console.log(eachProject);
           return (<Card key={eachProject.id} projectData={eachProject} />)
-          
         })}
+
       </div>
     </section>
   );
